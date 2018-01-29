7b508721d73d50ab3a8647d06b3e0111d9b045fc
junjun <junjun@baicizhan.com>
2018-01-29 14:47:46 +0800

    Initial Commit

---
+++ oc_learning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oc_learning
+//
+//  Created by junjun on 2018/1/29.
+//  Copyright © 2018年 junjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
